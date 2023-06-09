21fd7e327b36998efa3fa5dd0881c310f21f4efb
PC15\Admin <hoan2k2k2k@gmail.com>
2023-06-09 21:05:30 +0700

    no message

---
+++ Project_Inventory_Manager (2)/Project_Inventory_Manager/Project_Inventory_Manager/GameUIManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Inventory_Manager
+{
+    public class FeatureBase
+    {
+        public string featureName = "";
+        public string methodName = "";
+
+        public FeatureBase(string featureName, string methodName)
+        {
+            this.featureName = featureName;
+            this.methodName = methodName;
+        }
+    }
+
+    public class GameUIManager
+    {
+        
+
+        public void ShowMainMenu()
+        {
+            FeatureBase[] features = new FeatureBase[3];
+            features[0] = new FeatureBase("Quan ly Weapon", "WeaponPanel");
+            features[1] = new FeatureBase("Quan ly Cloth", "ClothPanel");
+            features[2] = new FeatureBase("Quan ly tat ca item", "AllItemPanel");
+
+            string key = ShowMenuBase("Chuong trinh quan ly kho do", features);
+        }
+
+        public void ShowLine(int element)
+        {
+            for (int i = 0; i < element; i++)
+            {
+                Console.Write("=");
+            }
+            Console.WriteLine();
+        }
+
+        public string ShowMenuBase(string title, FeatureBase[] feature)
+        {
+            Console.WriteLine(title);
+            ShowLine(30);
+            for (int i = 0; i < feature.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}: {feature[i].featureName}");
+            }
+            ShowLine(30);
+
+            Console.Write("Nhap lua chon: ");
+            return Console.ReadLine();
+        }
+    }
+}
---
+++ Project_Inventory_Manager (2)/Project_Inventory_Manager/Project_Inventory_Manager/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Inventory_Manager
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            GameUIManager uiManager = new GameUIManager();
+            uiManager.ShowMainMenu();
+        }
+    }
+}
